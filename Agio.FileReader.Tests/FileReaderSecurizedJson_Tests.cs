63823c187e767fdcf83f924a22c362cdab9ef2a0
somossis <somossisc@afsgo.com>
2018-04-07 18:36:56 +0200

    Version 9

---
+++ Agio.FileReader.Tests/FileReaderSecurizedJson_Tests.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agio.FileReader.Tests
+{
+    [TestClass]
+    public class FileReaderSecurizedJson_Tests
+    {
+        #region Constants
+
+        /// <summary>
+        /// The Admin role
+        /// </summary>
+        private const string ADMIN_ROLE = "Admin";
+
+        /// <summary>
+        /// The User role
+        /// </summary>
+        private const string USER_ROLE = "User";
+
+        /// <summary>
+        /// The path to the empty json file
+        /// </summary>
+        private const string EMPTY_JSON_FILEPATH = @"SampleFiles\empty.json";
+
+        /// <summary>
+        /// The path to the json file with content
+        /// </summary>
+        private const string CONTENT_JSON_FILEPATH = @"SampleFiles\content.json";
+
+        #endregion
+
+        #region Public Methods
+
+        [TestMethod]
+        public void Read_ContentJsonFileAdminRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var jsonReader = new FileReaderJson();
+            var reader = new FileReaderSecurizedJson(authorizer, jsonReader);
+
+            //Act
+            var result = reader.Read(ADMIN_ROLE, CONTENT_JSON_FILEPATH);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_ContentJsonFileAdminRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var jsonReader = new FileReaderJson();
+            var reader = new FileReaderSecurizedJson(authorizer, jsonReader);
+
+            //Act
+            var result = await reader.ReadAsync(ADMIN_ROLE, CONTENT_JSON_FILEPATH);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public void Read_ContentJsonFileUserRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var jsonReader = new FileReaderJson();
+            var reader = new FileReaderSecurizedJson(authorizer, jsonReader);
+
+            //Act and Assert
+            Assert.ThrowsException<UnauthorizedAccessException>(() => reader.Read(USER_ROLE, CONTENT_JSON_FILEPATH));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_ContentXmlFileUserRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var jsonReader = new FileReaderJson();
+            var reader = new FileReaderSecurizedJson(authorizer, jsonReader);
+
+            //Act and Assert
+            await Assert.ThrowsExceptionAsync<UnauthorizedAccessException>(async () =>
+            {
+                await reader.ReadAsync(USER_ROLE, CONTENT_JSON_FILEPATH);
+            });
+        }
+
+        [TestMethod]
+        public void Read_EmptyJsonFileAdminRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var jsonReader = new FileReaderJson();
+            var reader = new FileReaderSecurizedJson(authorizer, jsonReader);
+
+            //Act
+            var result = reader.Read(ADMIN_ROLE, EMPTY_JSON_FILEPATH);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_EmptyJsonFileAdminRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var jsonReader = new FileReaderJson();
+            var reader = new FileReaderSecurizedJson(authorizer, jsonReader);
+
+            //Act
+            var result = await reader.ReadAsync(ADMIN_ROLE, EMPTY_JSON_FILEPATH);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public void Read_EmptyJsonFileUserRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var jsonReader = new FileReaderJson();
+            var reader = new FileReaderSecurizedJson(authorizer, jsonReader);
+
+            //Act
+            var result = reader.Read(USER_ROLE, EMPTY_JSON_FILEPATH);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_EmptyJsonFileUserRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var jsonReader = new FileReaderJson();
+            var reader = new FileReaderSecurizedJson(authorizer, jsonReader);
+
+            //Act
+            var result = await reader.ReadAsync(USER_ROLE, EMPTY_JSON_FILEPATH);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrWhiteSpace(result));
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static IDictionary<string, IEnumerable<string>> GetPermissions()
+        {
+            var result = new Dictionary<string, IEnumerable<string>>
+            {
+                { EMPTY_JSON_FILEPATH, new List<string> { USER_ROLE, ADMIN_ROLE } },
+                { CONTENT_JSON_FILEPATH, new List<string> { ADMIN_ROLE } }
+            };
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ Agio.FileReader/FileReaderSecurizedJson.cs
@@ -0,0 +1,24 @@
+﻿using Agio.FileReader.Abstract;
+
+namespace Agio.FileReader
+{
+    /// <summary>
+    /// Implements a JSON file reader securized
+    /// </summary>
+    public sealed class FileReaderSecurizedJson : FileReaderSecurized<IAuthorizer, FileReaderJson>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the current class with the specified parameters
+        /// </summary>
+        /// <param name="authorizer">The authorizer to manage the file permissions whose should be read by the specified file reader</param>
+        /// <param name="fileReaderJson">The file reader to read the JSON files</param>
+        public FileReaderSecurizedJson(IAuthorizer authorizer, FileReaderJson fileReaderJson)
+            : base(authorizer, fileReaderJson)
+        {
+        }
+
+        #endregion
+    }
+}
