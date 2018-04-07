32b814937812230031f6988b1ed9a98f1f6adb59
somossis <somossisc@afsgo.com>
2018-04-07 13:07:20 +0200

    Version 2

---
+++ Agio.FileReader.Tests/FileReaderXml_Tests.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agio.FileReader.Tests
+{
+    [TestClass]
+    public class FileReaderXml_Tests
+    {
+        #region Constants
+
+        /// <summary>
+        /// The path of an empty text file
+        /// </summary>
+        private const string EMPTY_FILE_PATH = @"SampleFiles\Empty.xml";
+
+        /// <summary>
+        /// The path of a text file with some content
+        /// </summary>
+        private const string CONTENT_FILE_PATH = @"SampleFiles\Content.xml";
+
+        #endregion
+
+        #region Public Methods
+
+        [TestMethod]
+        public void Read_NullPath_Test()
+        {
+            //Arrange
+            var reader = new FileReaderXml();
+
+            //Act and Assert
+            Assert.ThrowsException<ArgumentNullException>(() => reader.Read(null));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_NullPath_Test()
+        {
+            //Arrange
+            var reader = new FileReaderXml();
+
+            //Act and Assert
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await reader.ReadAsync(null));
+        }
+
+        [TestMethod]
+        public void Read_EmptyFile_Test()
+        {
+            //Arrange
+            var path = EMPTY_FILE_PATH;
+            var reader = new FileReaderXml();
+
+            //Act
+            var content = reader.Read(path);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrEmpty(content));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_EmptyFile_Test()
+        {
+            //Arrange
+            var path = EMPTY_FILE_PATH;
+            var reader = new FileReaderXml();
+
+            //Act
+            var content = await reader.ReadAsync(path);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrEmpty(content));
+        }
+
+        [TestMethod]
+        public void Read_ContentFile_Test()
+        {
+            //Arrange
+            var path = CONTENT_FILE_PATH;
+            var reader = new FileReaderXml();
+
+            //Act
+            var content = reader.Read(path);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrEmpty(content));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_ContentFile_Test()
+        {
+            //Arrange
+            var path = CONTENT_FILE_PATH;
+            var reader = new FileReaderXml();
+
+            //Act
+            var content = await reader.ReadAsync(path);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrEmpty(content));
+        }
+
+        [TestMethod]
+        public void Read_IsInvalidXml_Test()
+        {
+            var path = EMPTY_FILE_PATH;
+            var reader = new FileReaderXml();
+            var xmlDoc = new XmlDocument();
+
+            //Act
+            var content = reader.Read(path);
+
+            //Assert
+            Assert.ThrowsException<XmlException>(() => xmlDoc.LoadXml(content));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_IsInvalidXml_Test()
+        {
+            //Arrange
+            var path = EMPTY_FILE_PATH;
+            var reader = new FileReaderXml();
+            var xmlDoc = new XmlDocument();
+
+            //Act
+            var content = await reader.ReadAsync(path);
+
+            //Assert
+            Assert.ThrowsException<XmlException>(() => xmlDoc.LoadXml(content));
+        }
+
+        [TestMethod]
+        public void Read_IsValidXml_Test()
+        {
+            var path = CONTENT_FILE_PATH;
+            var reader = new FileReaderXml();
+            var xmlDoc = new XmlDocument();
+
+            //Act
+            var content = reader.Read(path);
+            xmlDoc.LoadXml(content);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrEmpty(xmlDoc.OuterXml));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_IsValidXml_Test()
+        {
+            //Arrange
+            var path = CONTENT_FILE_PATH;
+            var reader = new FileReaderXml();
+            var xmlDoc = new XmlDocument();
+
+            //Act
+            var content = await reader.ReadAsync(path);
+            xmlDoc.LoadXml(content);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrEmpty(xmlDoc.OuterXml));
+        }
+
+        #endregion
+    }
+}
---
+++ Agio.FileReader/FileReaderXml.cs
@@ -0,0 +1,83 @@
+﻿using Agio.FileReader.Abstract;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Agio.FileReader
+{
+    /// <summary>
+    /// Implements a reader for XML files
+    /// </summary>
+    public sealed class FileReaderXml : IFileReader
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the current class
+        /// </summary>
+        public FileReaderXml()
+        {
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Reads the content of a XML file from the specified path
+        /// </summary>
+        /// <param name="path">Path to a text file</param>
+        /// <returns>The content of the text file as string</returns>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="FileNotFoundException"></exception>
+        /// <exception cref="NotSupportedException"></exception>
+        public string Read(string path)
+        {
+            try
+            {
+                var task = Task.Run(async () => await ReadAsync(path));
+
+                task.Wait();
+
+                return task.Result;
+            }
+            catch (AggregateException ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+
+        /// <summary>
+        /// Reads the content of a XML file from the specified path
+        /// </summary>
+        /// <remarks>This method runs asynchronously</remarks>
+        /// <param name="path">Path to a text file</param>
+        /// <returns>The content of the text file as string</returns>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="FileNotFoundException"></exception>
+        /// <exception cref="NotSupportedException"></exception>
+        public async Task<string> ReadAsync(string path)
+        {
+            var result = string.Empty;
+
+            //Note that a XML file still being a text file
+            using (var reader = File.OpenText(path))
+            {
+                //The xml file content could be encrypted or not
+                result = await reader.ReadToEndAsync();
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
