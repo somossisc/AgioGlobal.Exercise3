cd777ba592d457e16d5eceb2bcb8c178650d7ec7
somossis <somossisc@afsgo.com>
2018-04-07 16:02:39 +0200

    Version 6

---
+++ Agio.FileReader.Tests/FileReaderSecurizedText_Tests.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agio.FileReader.Tests
+{
+    [TestClass]
+    public class FileReaderSecurizedText_Tests
+    {
+        #region Constants
+
+            /// <summary>
+            /// The Admin role
+            /// </summary>
+        private const string ADMIN_ROLE = "Admin";
+
+        /// <summary>
+        /// The User role
+        /// </summary>
+        private const string USER_ROLE = "User";
+
+        /// <summary>
+        /// The path to the empty text file
+        /// </summary>
+        private const string EMPTY_TEXT_FILEPATH = @"SampleFiles\Empty.txt";
+
+        /// <summary>
+        /// The path to the text file with content
+        /// </summary>
+        private const string CONTENT_TEXT_FILEPATH = @"SampleFiles\Content.txt";
+
+        #endregion
+
+        #region Public Methods
+
+        [TestMethod]
+        public void Read_ContentTextFileAdminRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var textReader = new FileReaderText();
+            var reader = new FileReaderSecurizedText(authorizer, textReader);
+
+            //Act
+            var result = reader.Read(ADMIN_ROLE, CONTENT_TEXT_FILEPATH);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_ContentTextFileAdminRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var textReader = new FileReaderText();
+            var reader = new FileReaderSecurizedText(authorizer, textReader);
+
+            //Act
+            var result = await reader.ReadAsync(ADMIN_ROLE, CONTENT_TEXT_FILEPATH);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public void Read_ContentTextFileUserRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var textReader = new FileReaderText();
+            var reader = new FileReaderSecurizedText(authorizer, textReader);
+
+            //Act and Assert
+            Assert.ThrowsException<UnauthorizedAccessException>(() => reader.Read(USER_ROLE, CONTENT_TEXT_FILEPATH));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_ContentTextFileUserRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var textReader = new FileReaderText();
+            var reader = new FileReaderSecurizedText(authorizer, textReader);
+
+            //Act and Assert
+            await Assert.ThrowsExceptionAsync<UnauthorizedAccessException>(async () =>
+            {
+                await reader.ReadAsync(USER_ROLE, CONTENT_TEXT_FILEPATH);
+            });
+        }
+
+        [TestMethod]
+        public void Read_EmptyTextFileAdminRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var textReader = new FileReaderText();
+            var reader = new FileReaderSecurizedText(authorizer, textReader);
+
+            //Act
+            var result = reader.Read(ADMIN_ROLE, EMPTY_TEXT_FILEPATH);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_EmptyTextileAdminRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var textReader = new FileReaderText();
+            var reader = new FileReaderSecurizedText(authorizer, textReader);
+
+            //Act
+            var result = await reader.ReadAsync(ADMIN_ROLE, EMPTY_TEXT_FILEPATH);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public void Read_EmptyTextFileUserRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var textReader = new FileReaderText();
+            var reader = new FileReaderSecurizedText(authorizer, textReader);
+
+            //Act
+            var result = reader.Read(USER_ROLE, EMPTY_TEXT_FILEPATH);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrWhiteSpace(result));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_EmptyTextFileUserRoleAccess_Test()
+        {
+            //Arrange
+            var permissions = GetPermissions();
+            var authorizer = new Authorizer(permissions);
+            var textReader = new FileReaderText();
+            var reader = new FileReaderSecurizedText(authorizer, textReader);
+
+            //Act
+            var result = await reader.ReadAsync(USER_ROLE, EMPTY_TEXT_FILEPATH);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrWhiteSpace(result));
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static IDictionary<string, IEnumerable<string>> GetPermissions()
+        {
+            var result = new Dictionary<string, IEnumerable<string>>
+            {
+                { EMPTY_TEXT_FILEPATH, new List<string> { USER_ROLE, ADMIN_ROLE } },
+                { CONTENT_TEXT_FILEPATH, new List<string> { ADMIN_ROLE } }
+            };
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ Agio.FileReader/FileReaderSecurizedText.cs
@@ -0,0 +1,24 @@
+﻿using Agio.FileReader.Abstract;
+
+namespace Agio.FileReader
+{
+    /// <summary>
+    /// Implements a text file reader securized
+    /// </summary>
+    public sealed class FileReaderSecurizedText : FileReaderSecurized<IAuthorizer, FileReaderText>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the current class with the specified parameters
+        /// </summary>
+        /// <param name="authorizer">The authorizer to manage the file permissions whose should be read by the specified file reader</param>
+        /// <param name="fileReaderText">The file reader to read the text files</param>
+        public FileReaderSecurizedText(IAuthorizer authorizer, FileReaderText fileReaderText)
+            : base(authorizer, fileReaderText)
+        {
+        }
+
+        #endregion
+    }
+}
