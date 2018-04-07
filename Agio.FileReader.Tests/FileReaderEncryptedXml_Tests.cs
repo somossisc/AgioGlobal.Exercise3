93ed4628556cc7574351a6fc6a42387104387aa0
somossis <somossisc@afsgo.com>
2018-04-07 15:32:50 +0200

    Version 5

---
+++ Agio.FileReader.Tests/FileReaderEncryptedXml_Tests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Agio.FileReader.Tests
+{
+    [TestClass]
+    public class FileReaderEncryptedXml_Tests
+    {
+        #region Constants
+
+        /// <summary>
+        /// The path of an encrypted empty text file
+        /// </summary>
+        private const string ENCRYPTED_EMPTY_XMLFILE_PATH = @"SampleFiles\Empty.xml";
+
+        /// <summary>
+        /// The path of a text file with encrypted content
+        /// </summary>
+        private const string ENCRYPTED_CONTENT_TEXTFILE_PATH = @"SampleFiles\ContentEncrypted.xml";
+
+        /// <summary>
+        /// The path of a text file with decrypted content
+        /// </summary>
+        private const string DECRYPTED_CONTENT_XMLFILE_PATH = @"SampleFiles\ContentDecrypted.xml";
+
+        #endregion
+
+        #region Public Methods
+
+        [TestMethod]
+        public void ReadEncrypted_EmptyXmlFile_Test()
+        {
+            //Arrange
+            var encrypter = new Encrypter();
+            var xmlReader = new FileReaderXml();
+            var reader = new FileReaderEncryptedXml(encrypter, xmlReader);
+
+            //Act
+            var result = reader.ReadEncrypted(ENCRYPTED_EMPTY_XMLFILE_PATH);
+
+            //Assert
+            Assert.IsTrue(string.Equals(result, string.Empty));
+        }
+
+        [TestMethod]
+        public async Task ReadEncryptedAsync_EmptyXmlFile_Test()
+        {
+            //Arrange
+            var encrypter = new Encrypter();
+            var xmlReader = new FileReaderXml();
+            var reader = new FileReaderEncryptedXml(encrypter, xmlReader);
+
+            //Act
+            var result = await reader.ReadEncryptedAsync(ENCRYPTED_EMPTY_XMLFILE_PATH);
+
+            //Assert
+            Assert.IsTrue(string.Equals(result, string.Empty));
+        }
+
+        [TestMethod]
+        public void ReadEncrypted_EncryptedContentXmlFile_Test()
+        {
+            //Arrange
+            var encrypter = new Encrypter();
+            var xmlReader = new FileReaderXml();
+            var reader = new FileReaderEncryptedXml(encrypter, xmlReader);
+            var decryptedContent = xmlReader.Read(DECRYPTED_CONTENT_XMLFILE_PATH);
+
+            //Act
+            var result = reader.ReadEncrypted(ENCRYPTED_CONTENT_TEXTFILE_PATH);
+
+            //Assert
+            Assert.IsTrue(string.Equals(result, decryptedContent));
+        }
+
+        [TestMethod]
+        public async Task ReadEncryptedAsync_EncryptedContentXmlFile_Test()
+        {
+            //Arrange
+            var encrypter = new Encrypter();
+            var xmlReader = new FileReaderXml();
+            var reader = new FileReaderEncryptedXml(encrypter, xmlReader);
+            var decryptedContent = await xmlReader.ReadAsync(DECRYPTED_CONTENT_XMLFILE_PATH);
+
+            //Act
+            var result = await reader.ReadEncryptedAsync(ENCRYPTED_CONTENT_TEXTFILE_PATH);
+
+            //Assert
+            Assert.IsTrue(string.Equals(result, decryptedContent));
+        }
+
+        #endregion
+    }
+}
---
+++ Agio.FileReader/FileReaderEncryptedXml.cs
@@ -0,0 +1,21 @@
+﻿using Agio.FileReader.Abstract;
+
+namespace Agio.FileReader
+{
+    public sealed class FileReaderEncryptedXml : FileReaderEncrypted<IEncrypter, FileReaderXml>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the current class with the specified parameters
+        /// </summary>
+        /// <param name="encrypter">The encrypter to decrypt the content fo the encrypted files</param>
+        /// <param name="fileReaderXml">The reader to read the encrypted xml files</param>
+        public FileReaderEncryptedXml(IEncrypter encrypter, FileReaderXml fileReaderXml)
+            : base(encrypter, fileReaderXml)
+        {
+        }
+
+        #endregion
+    }
+}
