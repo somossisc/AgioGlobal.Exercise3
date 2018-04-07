bd127d4f295f528a6f93d9908778818cfe9e49b5
somossis <somossisc@afsgo.com>
2018-04-07 16:29:10 +0200

    Version 7

---
+++ Agio.FileReader.Tests/FileREaderJson_Tests.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Json;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agio.FileReader.Tests
+{
+    [TestClass]
+    public class FileReaderJson_Tests
+    {
+        #region Constants
+
+        /// <summary>
+        /// The path of an empty JSON file
+        /// </summary>
+        private const string EMPTY_FILE_PATH = @"SampleFiles\empty.json";
+
+        /// <summary>
+        /// The path of a JSON file with some content
+        /// </summary>
+        private const string CONTENT_FILE_PATH = @"SampleFiles\content.json";
+
+        #endregion
+
+        #region Public Methods
+
+        [TestMethod]
+        public void Read_NullPath_Test()
+        {
+            //Arrange
+            var reader = new FileReaderJson();
+
+            //Act and Assert
+            Assert.ThrowsException<ArgumentNullException>(() => reader.Read(null));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_NullPath_Test()
+        {
+            //Arrange
+            var reader = new FileReaderJson();
+
+            //Act and Assert
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await reader.ReadAsync(null));
+        }
+
+        [TestMethod]
+        public void Read_EmptyFile_Test()
+        {
+            //Arrange
+            var path = EMPTY_FILE_PATH;
+            var reader = new FileReaderJson();
+
+            //Act
+            var content = reader.Read(path);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrEmpty(content));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_EmptyFile_Test()
+        {
+            //Arrange
+            var path = EMPTY_FILE_PATH;
+            var reader = new FileReaderJson();
+
+            //Act
+            var content = await reader.ReadAsync(path);
+
+            //Assert
+            Assert.IsTrue(string.IsNullOrEmpty(content));
+        }
+
+        [TestMethod]
+        public void Read_ContentFile_Test()
+        {
+            //Arrange
+            var path = CONTENT_FILE_PATH;
+            var reader = new FileReaderJson();
+
+            //Act
+            var content = reader.Read(path);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrEmpty(content));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_ContentFile_Test()
+        {
+            //Arrange
+            var path = CONTENT_FILE_PATH;
+            var reader = new FileReaderJson();
+
+            //Act
+            var content = await reader.ReadAsync(path);
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrEmpty(content));
+        }
+
+        [TestMethod]
+        public void Read_IsInvalidJson_Test()
+        {
+            var path = EMPTY_FILE_PATH;
+            var reader = new FileReaderJson();
+
+            //Act
+            var content = reader.Read(path);
+
+            //Assert
+            Assert.ThrowsException<ArgumentException>(() => JsonValue.Parse(content));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_IsInvalidJson_Test()
+        {
+            //Arrange
+            var path = EMPTY_FILE_PATH;
+            var reader = new FileReaderJson();
+            
+            //Act
+            var content = await reader.ReadAsync(path);
+
+            //Assert
+            Assert.ThrowsException<ArgumentException>(() => JsonValue.Parse(content));
+        }
+
+        [TestMethod]
+        public void Read_IsValidJson_Test()
+        {
+            var path = CONTENT_FILE_PATH;
+            var reader = new FileReaderJson();
+
+            //Act
+            var content = reader.Read(path);
+
+            //Assert
+            Assert.IsNotNull(JsonValue.Parse(content));
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_IsValidJson_Test()
+        {
+            //Arrange
+            var path = CONTENT_FILE_PATH;
+            var reader = new FileReaderJson();
+
+            //Act
+            var content = await reader.ReadAsync(path);
+
+            //Assert
+            Assert.IsNotNull(JsonValue.Parse(content));
+        }
+
+        #endregion
+    }
+}
---
+++ Agio.FileReader/FileReaderJson.cs
@@ -0,0 +1,84 @@
+﻿using Agio.FileReader.Abstract;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Agio.FileReader
+{
+    /// <summary>
+    /// Implements a reader for JSON files
+    /// </summary>
+    public sealed class FileReaderJson : IFileReader
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the current class
+        /// </summary>
+        public FileReaderJson()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Reads the content of a JSON file from the specified path
+        /// </summary>
+        /// <param name="path">Path to a JSON file</param>
+        /// <returns>The content of the JSON file as string</returns>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="FileNotFoundException"></exception>
+        /// <exception cref="NotSupportedException"></exception>
+        public string Read(string path)
+        {
+            try
+            {
+                var task = Task.Run(async () => await ReadAsync(path));
+
+                task.Wait();
+
+                return task.Result;
+            }
+            catch (AggregateException ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+
+        /// <summary>
+        /// Reads the content of a JSON file from the specified path
+        /// </summary>
+        /// <remarks>This method runs asynchronously</remarks>
+        /// <param name="path">Path to a JSON file</param>
+        /// <returns>The content of the JSON file as string</returns>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="FileNotFoundException"></exception>
+        /// <exception cref="NotSupportedException"></exception>
+        public async Task<string> ReadAsync(string path)
+        {
+            var result = string.Empty;
+
+            //Note that a JSON file still being a text file
+            using (var reader = File.OpenText(path))
+            {
+                //The JSON file content could be encrypted or not
+                result = await reader.ReadToEndAsync();
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
